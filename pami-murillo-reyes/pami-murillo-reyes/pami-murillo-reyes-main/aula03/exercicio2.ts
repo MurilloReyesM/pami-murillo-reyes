50609c0ec9f6a01cf41b52323c72706537a9be29
MurilloReyesM <reyesmrm@outlook.com>
2026-09-24 22:15:44 -0300

    Delete pami-murillo-reyes directory
    Delete pami-murillo-reyes directory

--- pami-murillo-reyes/pami-murillo-reyes/pami-murillo-reyes-main/aula03/exercicio2.ts
+++
@@ -1,9 +0,0 @@
-const cidades: string[] = ['São Paulo', 'Curitiba', 'Salvador', 'Belo Horizonte', 'Recife'];
-
-function listarCidades(lista: string[]): void {
-  lista.forEach((cidade) => {
-    console.log(cidade);
-  });
-}
-
-listarCidades(cidades);
